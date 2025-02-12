aa0661cedf752da70eb3f6ea9ac8770f430c4754
Dell <Dell@DESKTOP-G8FGSQD>
2025-02-12 13:47:13 +0530

    I ahve made some Changes

--- FrameworkSrikanta/src/test/java/contactModule/ToCreateContactTest.java
+++ FrameworkSrikanta/src/test/java/contactModule/ToCreateContactTest.java
@@ -4,6 +4,7 @@ public class ToCreateContactTest {
 	public static void main(String[] args) {
 
 		System.out.println("write test case for contact module");
-
-	}
+		//This is My Code
+		System.out.println("hi This is chandra");
+		}
 }
--- FrameworkSrikanta/src/test/java/organisationModule/ToCreateOrganisationTest.java
+++ FrameworkSrikanta/src/test/java/organisationModule/ToCreateOrganisationTest.java
@@ -3,5 +3,7 @@ package organisationModule;
 public class ToCreateOrganisationTest {
 	public static void main(String[] args) {
 		System.out.println("write the test case for organisation module");
+		//This is My Code
+		System.out.println("hi This is chandra");
 	}
 }
