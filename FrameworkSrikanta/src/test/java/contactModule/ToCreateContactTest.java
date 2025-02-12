51286098e787434619507e1225d023032b7e54a3
Saumya <saumyaranjanparida175@gmail.com>
2025-02-12 14:23:52 +0530

    Saumya- first push

--- FrameworkSrikanta/src/test/java/contactModule/ToCreateContactTest.java
+++ FrameworkSrikanta/src/test/java/contactModule/ToCreateContactTest.java
@@ -6,5 +6,7 @@ public class ToCreateContactTest {
 		System.out.println("write test case for contact module");
 		//This is My Code
 		System.out.println("hi This is chandra");
-		}
+		//
+		System.out.println("HY THIS IS MY CODE SAUMYA");
+	}
 }
